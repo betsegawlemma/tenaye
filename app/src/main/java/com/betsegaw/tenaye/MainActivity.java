9539dc2a1f1496e6c97639a1027e3cb8dd20b168
Betsegaw Lemma <betsegawlemma@gmail.com>
2018-03-12 11:36:14 +0300

    use strings.xml to store data in place of db

--- app/src/main/java/com/betsegaw/tenaye/MainActivity.java
+++ app/src/main/java/com/betsegaw/tenaye/MainActivity.java
@@ -1,29 +1,35 @@
 package com.betsegaw.tenaye;
 
-import android.arch.persistence.db.SupportSQLiteDatabase;
-import android.arch.persistence.room.Room;
-import android.arch.persistence.room.RoomDatabase;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.betsegaw.tenaye.database.AppDatabase;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      //  Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(myToolbar);
+
+      //  myToolbar.setLogo(R.drawable.tenayelogo);
+      // myToolbar.setSubtitle("ToolBar");
+
         //database connection
-        Room.databaseBuilder(getApplicationContext(),
+       /* Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "database-name.db")
                 .addCallback(new RoomDatabase.Callback() {
                     @Override
                     public void onCreate(@NonNull SupportSQLiteDatabase db) {
                         super.onCreate(db);
+                       *//* new Thread(()->{
+                            db.insert(new Category(1,"HIV","HIV","HIV"));
+                        }).start();*//*
                     }
-                });
+                }).build();*/
 
         setContentView(R.layout.activity_main);
     }
+
+
 }
